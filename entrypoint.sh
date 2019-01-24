c22a4705ee3560a2630d7fe4ca5ad2ae174ed050
Joshua Rubin <joshua@rubixconsulting.com>
2019-01-24 16:33:36 -0700

    GitHub Pages doesn't seem to like force pushes
    So... we'll do a merge instead
    
    Signed-off-by: Joshua Rubin <joshua@rubixconsulting.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,11 +13,13 @@ cd public
 remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 remote_branch=${BRANCH:-gh-pages}
 git init
+git remote add origin "${remote_repo}"
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add .
-printf "Files to Commit: %d\n" "$(find . -type f | wc -l)"
-git commit -m "Automated deployment to GitHub Pages on $(date +%s%3N)"
-git push --force "$remote_repo" "master:$remote_branch"
+git commit -m "Automated deployment to GitHub Pages at $(date +%s%3N)"
+git fetch origin
+git merge -s ours --allow-unrelated-histories -m "Merge remote-tracking branch 'origin/${remote_branch}'" "origin/${remote_branch}" || true
+git push origin "master:$remote_branch"
 
 echo '👍 GREAT SUCCESS!'
