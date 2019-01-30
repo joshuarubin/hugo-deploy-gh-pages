427ad6de6166845c68684864c916e4138c228d84
Joshua Rubin <joshua@rubixconsulting.com>
2019-01-30 11:14:10 -0700

    avoid merging for cleaner history
    Signed-off-by: Joshua Rubin <joshua@rubixconsulting.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,35 @@
 #!/bin/sh -eu
 
+remote_repo="https://${PAGES_PUSH_USERNAME}:${PAGES_PUSH_ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+remote_branch=${BRANCH:-gh-pages}
+export HUGO_ENV=production
+
 echo '👍 ENTRYPOINT HAS STARTED—UPDATING SUBMODULES'
 git submodule init
 git submodule update --recursive --remote
 
-echo '👍 UPDATED SUBMODULES—BUILDING THE SITE'
-HUGO_ENV=production hugo -v --minify
+echo '👍 UPDATED SUBMODULES—PREPARING THE REPO'
+
+(
+  mkdir public
+  cd public
+  git init
+  git remote add origin "${remote_repo}"
+  git config user.name "${GITHUB_ACTOR}"
+  git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+  git pull origin "${remote_branch}"
+  git rm -rf .
+)
+
+echo '👍 PREPARED THE REPO—BUILDING THE SITE'
+hugo -v --minify
 
 echo '👍 THE SITE IS BUILT—PUSHING IT BACK TO GITHUB-PAGES'
-cd public
-remote_repo="https://${PAGES_PUSH_USERNAME}:${PAGES_PUSH_ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-remote_branch=${BRANCH:-gh-pages}
-git init
-git remote add origin "${remote_repo}"
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git add .
-git commit -m "Automated deployment to GitHub Pages at $(date +%s%3N)"
-git fetch origin
-git merge -s ours --allow-unrelated-histories -m "Merge remote-tracking branch 'origin/${remote_branch}'" "origin/${remote_branch}" || true
-git push origin "master:${remote_branch}"
+(
+  cd public
+  git add .
+  git commit -m "Automated deployment to GitHub Pages at $(date +%s%3N)" --allow-empty
+  git push origin "master:${remote_branch}"
+)
 
 echo '👍 GREAT SUCCESS!'
